4cfb64aa3f0e992a6f444b3d5735938eb1ecc0eb
icykoneko <icykoneko@users.noreply.github.com>
2019-04-06 23:31:11 -0400

    Added `forcast` to the `WeatherRate`.

--- xiv/weather_rate.py
+++ xiv/weather_rate.py
@@ -1,12 +1,17 @@
 from typing import Iterable, List, Tuple, cast
+from datetime import timedelta
+from itertools import dropwhile
 
 from xiv import register_xivrow, XivRow, IXivSheet
 from ex.relational import IRelationalRow
+from eorzeadatetime import EorzeaDateTime
 
 from xiv.weather import Weather
 
 
 class WeatherRate(XivRow):
+    WEATHER_CHANGE_INTERVAL = timedelta(hours=8)
+
     @property
     def possible_weathers(self) -> Iterable[Weather]:
         return self._possible_weathers
@@ -33,5 +38,32 @@ class WeatherRate(XivRow):
 
         self._possible_weathers = list(set(w))
         self._weather_rates = wr
+
+    def forcast(self, time: EorzeaDateTime) -> Weather:
+        def calc_target(time):
+            # Generate a number between [0..99] based on time.
+            # Convert the Eorzea date/time into Unix Time.
+            unix_time = time.get_unix_time()
+            # Get the Eorzea hour for weather start.
+            bell = int(unix_time / 175)
+            # Magic needed for calculations:
+            # 16:00 = 0, 00:00 = 8, 08:00 = 16 . . .
+            inc = ((bell + 8 - (bell % 8))) % 24
+            # Take the Eorzea days since Unix Epoch.
+            total_days = int(unix_time / 4200)
+
+            # Make the calculations
+            calc_base = (total_days * 100) + inc
+            step1 = ((calc_base << 11) & 0xFFFFFFFF) ^ calc_base
+            step2 = (step1 >> 8) ^ step1
+
+            return (step2 % 100) & 0xFFFFFFFF
+
+        target = calc_target(time)
+        forcasted_weather = next(dropwhile(lambda x: target >= x[0], self._weather_rates), None)
+        if forcasted_weather is not None:
+            return forcasted_weather[1]
+        else:
+            return None
 
 register_xivrow(WeatherRate)
